1269d567a7a910ff3013b3b0750f87e307b3d422
Hao Lin <linhaomails@gmail.com>
2025-10-11 08:01:29 +0000

    fix: file contention when multiple libero instances init
    When multiple libero instances run for the first time, they all try to
    create the config directory and file, causing contention.
    This commit adds a file lock to prevent contention
    
    Signed-off-by: Hao Lin <linhaomails@gmail.com>

--- libero/libero/__init__.py
+++ libero/libero/__init__.py
@@ -1,11 +1,13 @@
 import os
 import yaml
+from filelock import FileLock
 
 # This is a default path for localizing all the benchmark related files
 libero_config_path = os.environ.get(
     "LIBERO_CONFIG_PATH", os.path.expanduser("~/.libero")
 )
 config_file = os.path.join(libero_config_path, "config.yaml")
+config_file_lock = os.path.join(libero_config_path, ".config_lock")
 
 
 def get_default_path_dict(custom_location=None):
@@ -52,45 +54,25 @@ def get_libero_path(query_key):
 
 def set_libero_default_path(custom_location=os.path.dirname(os.path.abspath(__file__))):
     print(
-        f"[Warning] You are changing the default path for Libero config. This will affect all the paths in the config file."
+        "[Warning] You are changing the default path for Libero config. This will affect all the paths in the config file."
     )
     new_config = get_default_path_dict(custom_location)
     with open(config_file, "w") as f:
         yaml.dump(new_config, f)
 
-
-if not os.path.exists(libero_config_path):
-    os.makedirs(libero_config_path)
+with FileLock(config_file_lock):
+    if not os.path.exists(libero_config_path):
+        os.makedirs(libero_config_path)
 
 if not os.path.exists(config_file):
     # Create a default config file
 
     default_path_dict = get_default_path_dict()
-    answer = input(
-        "Do you want to specify a custom path for the dataset folder? (Y/N): "
-    ).lower()
-    if answer == "y":
-        # If the user wants to specify a custom storage path, prompt them to enter it
-        custom_dataset_path = input(
-            "Enter the path where you want to store the datasets: "
-        )
-        full_custom_dataset_path = os.path.join(
-            os.path.abspath(os.path.expanduser(custom_dataset_path)), "datasets"
-        )
-        # Check if the custom storage path exists, and create if it doesn't
-
-        print("The full path of the custom storage path you entered is:")
-        print(full_custom_dataset_path)
-        print("Do you want to continue? (Y/N)")
-        confirm_answer = input().lower()
-        if confirm_answer == "y":
-            if not os.path.exists(full_custom_dataset_path):
-                os.makedirs(full_custom_dataset_path)
-            default_path_dict["datasets"] = full_custom_dataset_path
     print("Initializing the default config file...")
     print(f"The following information is stored in the config file: {config_file}")
     # write all the paths into a yaml file
-    with open(config_file, "w") as f:
-        yaml.dump(default_path_dict, f)
-    for key, value in default_path_dict.items():
-        print(f"{key}: {value}")
+    with FileLock(config_file_lock):
+        with open(config_file, "w") as f:
+            yaml.dump(default_path_dict, f)
+        for key, value in default_path_dict.items():
+            print(f"{key}: {value}")
